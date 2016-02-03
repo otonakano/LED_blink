de04b0c813dd8c9b5e90d7ec5adba662b9467c1b
oto nakano <nakanooto@nakanooto-no-Mac-mini.local>
2016-02-03 22:49:54 +0900

    1.0.0
    Arduino Leonardoで、基板上のLEDを点滅させるスケッチ。

---
+++ sketch_feb03-LED100.ino
@@ -0,0 +1,23 @@
+void LED_blink(int count, int interval);
+
+void setup(){
+  pinMode(13,OUTPUT);
+}
+
+void loop(){
+  LED_blink(10, 300);
+  LED_blink(5,200);
+  LED_blink(3,100);
+}
+
+void LED_blink(int count, int interval){
+  int i = 0;
+  while(i < count){
+    digitalWrite(13,HIGH);
+    delay(interval);
+    digitalWrite(13,LOW);
+    delay(interval);
+    i++;
+  }
+}
+
